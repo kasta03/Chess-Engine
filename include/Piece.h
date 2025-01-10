b00c51d678b180c226362f3945705d6fdc33dcbb
Karol Starzyk <kastarzyk03@gmail.com>
2025-01-10 20:18:54 +0100

    developing white pawn

--- include/Piece.h
+++ include/Piece.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <array>
+#include <utility>
+#include <vector>
 #include <iostream>
 
 using U64 = unsigned long long;
 
 class Piece
 {
+    U64 white_pieces_mask = 0;
+    U64 black_pieces_mask = 0;
+    U64 all_pieces_mask = white_pieces_mask | black_pieces_mask;
     public:
 
 };
--- include/WhitePawn.h
+++ include/WhitePawn.h
@@ -3,20 +3,54 @@
 
 class WhitePawn
 {
-    private:
+private:
     U64 white_pawns_position = 0b11111111 << 8;
-    std::array<U64, 64> white_pawn_pre_moves{1};
-    
-    public:
+    std::array<U64, 64> white_pawn_pre_moves;
+    std::array<U64, 64> white_pawn_pre_attacks;
+    std::array<U64, 64> white_pawn_positions;
+    std::vector<std::pair<int, int>> white_pawn_moves;
+
+public:
     void CalculateWhitePawnsPreMoves()
     {
-        for (int i = 8; i < 64; ++i)
+        for (int i = 8; i < 56; ++i)
         {
-            white_pawn_pre_moves[i] <<= 8; 
+            white_pawn_pre_moves[i] = 1ULL << (8 + i);
             if (i / 8 == 1)
             {
-                white_pawn_pre_moves[i] = white_pawn_pre_moves[i] & white_pawn_pre_moves[i] << 8; 
+                white_pawn_pre_moves[i] |= 1ULL << (16 + i);
             }
+        }
+    }
+    void CalculateWhitePawnsPreAttacks()
+    {
+        for (int i = 8; i < 56; ++i)
+        {
+            if (i % 8 != 0)
+            {
+                white_pawn_pre_attacks[i] |= 1ULL << (7 + i);
+            }
+            if (i % 8 != 7)
+            {
+                white_pawn_pre_attacks[i] |= 1ULL << (9 + i);
+            }
+        }
+    }
+    void getPawnPositions()
+    {
+        for (int i = 0; i < 64; ++i)
+        {
+            white_pawn_positions[i] = white_pawns_position & (1ULL << i);
+        }
+    }
+    std::vector<std::pair<int, int>> CalculateWhitePawnMoves()
+    {
+        getPawnPositions();
+        int steps = white_pawn_positions.size();
+        int from; int to;
+        for (int i = 0; i < steps; ++i)
+        {
+            
         }
     }
 };
